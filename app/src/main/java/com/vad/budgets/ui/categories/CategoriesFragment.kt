d464633fd3d40ba2619dcc4bb1e3bf7231709907
Liem Vo <liemvt@gmail.com>
2021-06-21 21:23:52 +1200

    first commit

---
+++ app/src/main/java/com/vad/budgets/ui/categories/CategoriesFragment.kt
@@ -0,0 +1,31 @@
+package com.vad.budgets.ui.categories
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.vad.budgets.R
+
+class CategoriesFragment : Fragment() {
+    
+    private lateinit var categoriesViewModel: CategoriesViewModel
+    
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        categoriesViewModel =
+            ViewModelProvider(this).get(CategoriesViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_categories, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        categoriesViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ app/src/main/java/com/vad/budgets/ui/transactions/TransactionsViewModel.kt
@@ -0,0 +1,13 @@
+package com.vad.budgets.ui.transactions
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TransactionsViewModel : ViewModel() {
+    
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is transactions Fragment"
+    }
+    val text: LiveData<String> = _text
+}
