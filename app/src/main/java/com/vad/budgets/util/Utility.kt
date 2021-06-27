65a228969ebd9e1258634cf2aa4c73542a478014
Liem Vo <liemvt@gmail.com>
2021-06-27 20:41:47 +1200

    create start and end date time of the a specific month and year

--- app/src/main/java/com/vad/budgets/util/Utility.kt
+++ app/src/main/java/com/vad/budgets/util/Utility.kt
@@ -1,9 +1,52 @@
 package com.vad.budgets.util
 
 import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+
 
 object Utility {
+    private val calendar = Calendar.getInstance()
+    private const val DATE_FORMAT = "MMM, dd YYYY"
+
     val numberFormat: NumberFormat = NumberFormat.getInstance().apply {
         maximumFractionDigits = 2
+    }
+
+    fun getFirstDateTimeOfMonth(month: Int?, year: Int? = null): Date {
+        calendar.time = Date()
+        year?.let {
+            calendar.set(Calendar.YEAR, it)
+        }
+        month?.let {
+            calendar.set(Calendar.MONTH, month)
+        }
+        calendar.set(Calendar.DAY_OF_MONTH, 1)
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        return calendar.time
+    }
+
+    fun getLastDateTimeOfMonth(month: Int?, year: Int? = null): Date {
+        calendar.time = Date()
+        year?.let {
+            calendar.set(Calendar.YEAR, it)
+        }
+
+        val localMonth = month ?: calendar.get(Calendar.MONTH)
+        calendar.set(calendar.get(Calendar.YEAR), localMonth,
+            calendar.getActualMaximum(Calendar.DAY_OF_MONTH))
+        calendar.set(Calendar.HOUR_OF_DAY, 23)
+        calendar.set(Calendar.MINUTE, 59)
+        calendar.set(Calendar.SECOND, 59)
+        return calendar.time
+    }
+
+    fun Date.format(pattern: String = DATE_FORMAT): String {
+        val format = SimpleDateFormat(pattern, Locale.getDefault())
+        return format.format(this)
     }
 }
---
+++ app/src/test/java/com/vad/budgets/util/UtilityTest.kt
@@ -0,0 +1,19 @@
+package com.vad.budgets.util
+
+import com.vad.budgets.util.Utility.format
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class UtilityTest{
+    @Test
+    fun firstDateOfTheMonthInFormat() {
+        val date = Utility.getFirstDateTimeOfMonth(5, 2021)
+        assertEquals("Jun, 01 2021", date.format())
+    }
+
+    @Test
+    fun lastDateOfTheMonthInFormat() {
+        val date = Utility.getLastDateTimeOfMonth(5, 2021)
+        assertEquals("Jun, 30 2021", date.format())
+    }
+}
