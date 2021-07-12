3c3656a739daa369edf161a481e1f83976f45b82
Liem Vo <liemvt@gmail.com>
2021-07-13 09:38:52 +1200

    Update the repo with correct thread

--- app/src/main/java/com/vad/budgets/data/repository/CategoryRepository.kt
+++ app/src/main/java/com/vad/budgets/data/repository/CategoryRepository.kt
@@ -9,7 +9,6 @@ import javax.inject.Singleton
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
@@ -25,11 +24,13 @@ interface CategoryRepository {
 class CategoryRepositoryImpl @Inject constructor(
     private val categoryDao: CategoryDao,
 ) : CategoryRepository {
-    override fun getAllCategory(): Flow<WorkStatus<List<Category>>> = categoryDao.getAllCategoriesLiveData().asFlow()
-        .map { list -> WorkStatus.Success(list) }
-        .onStart<WorkStatus<List<Category>>> { emit(WorkStatus.Loading(null)) }
-        .catch { exception -> emit(WorkStatus.Error(exception = exception)) }
-        .flowOn(Dispatchers.IO)
+    override fun getAllCategory(): Flow<WorkStatus<List<Category>>> =
+        categoryDao.getAllCategoriesLiveData().asFlow()
+            .flowOn(Dispatchers.IO)
+            .map { list -> WorkStatus.Success(list) }
+            .onStart<WorkStatus<List<Category>>> { emit(WorkStatus.Loading(null)) }
+            .catch { exception -> emit(WorkStatus.Error(exception = exception)) }
+            .flowOn(Dispatchers.Default)
 
 
     override suspend fun insertCategory(category: Category) {
